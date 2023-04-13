36edeb55d53efeebee59bd2da02290c739fa2985
kristish000 <kristish000@gmail.com>
2023-04-14 00:20:43 +0200

    code rework

---
+++ copyscript.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("#task-form")
+const input = document.querySelector("#task-input")
+const list = document.querySelector("#tasks")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const task = input.value
+  if (!task) {
+    return
+  }
+
+  const task_div = document.createElement("div")
+  task_div.classList.add("task")
+  list.appendChild(task_div)
+
+  const task_content_div = document.createElement("div")
+  task_content_div.classList.add("content")
+  task_div.appendChild(task_content_div)
+
+  const task_input = document.createElement("input")
+  task_input.classList.add("text")
+  task_input.type = "text"
+  task_input.value = task
+  task_input.setAttribute("readonly", "readonly")
+  task_content_div.appendChild(task_input)
+
+  const task_actions = document.createElement("div")
+  task_actions.classList.add("actions")
+  task_div.appendChild(task_actions)
+
+  const editbtn = document.createElement("button")
+  editbtn.classList.add("edit")
+  editbtn.innerHTML = "Edit"
+
+  const delbtn = document.createElement("button")
+  delbtn.classList.add("delete")
+  delbtn.innerHTML = "Delete"
+
+  const completebtn = document.createElement("button")
+  completebtn.classList.add("complete")
+  completebtn.innerHTML = "Complete"
+  task_actions.appendChild(editbtn)
+  task_actions.appendChild(delbtn)
+  task_actions.appendChild(completebtn)
+
+  input.value = ""
+})
